5efbeb185022ab9f421931ca415ce10a6ea2d4d5
TenPlus1 <kinsellaja@yahoo.com>
2019-02-16 15:12:29 +0000

    almost forgot sapling abm check for sakuras :P

--- sapling.lua
+++ sapling.lua
@@ -209,7 +209,10 @@ local grow_sapling = function(pos, node)
 	elseif node.name == "ethereal:birch_sapling"
 	and under == "default:dirt_with_grass" then
 		ethereal.grow_birch_tree(pos)
-	end
+	elseif node.name == "ethereal:sakura_sapling"
+	and under == "ethereal:bamboo_dirt" then
+		ethereal.grow_sakura_tree(pos)
+    end
 
 end
 
