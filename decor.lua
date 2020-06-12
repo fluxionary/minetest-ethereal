b2082ccf24f40f21b90ac6f881fb2c139b570d81
TenPlus1 <kinsellaja@yahoo.com>
2020-06-12 15:04:48 +0100

    added wild cotton to savanna

--- decor.lua
+++ decor.lua
@@ -1,4 +1,25 @@
 
+if minetest.registered_nodes["farming:cotton_wild"] then
+minetest.register_decoration({
+	name = "farming:cotton_wild",
+	deco_type = "simple",
+	place_on = {"default:dry_dirt_with_dry_grass"},
+	sidelen = 16,
+	noise_params = {
+		offset = -0.1,
+		scale = 0.1,
+		spread = {x = 50, y = 50, z = 50},
+		seed = 4242,
+		octaves = 3,
+		persist = 0.7
+	},
+	biomes = {"savanna"},
+	y_max = 31000,
+	y_min = 1,
+	decoration = "farming:cotton_wild",
+})
+end
+
 -- water pools in swamp areas if 5.0 detected
 if minetest.registered_nodes["default:permafrost"] then
 minetest.register_decoration({
