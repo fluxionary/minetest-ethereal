a072437b59d7b023e037d7c49d935a6ab31701a5
tenplus1 <kinsellaja@yahoo.com>
2021-01-16 11:13:32 +0000

    added lemonade

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ Ethereal Mapgen mod for Minetest (works on all except v6)
  - Added new Meditteranean biome with Lemon and Olive trees (thanks Felfa)
  - Added Candied Lemon and Olive Oil items and recipe
  - Rewrite stairs and add auto-select function
+ - Added Lemonade
 
 ### 1.27
 
--- food.lua
+++ food.lua
@@ -322,6 +322,36 @@ minetest.register_craft({
 	},
 })
 
+-- Lemonade
+minetest.register_node("ethereal:lemonade", {
+	description = S("Lemonade"),
+	drawtype = "plantlike",
+	tiles = {"ethereal_lemonade.png"},
+	inventory_image = "ethereal_lemonade.png",
+	wield_image = "ethereal_lemonade.png",
+	paramtype = "light",
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.25, -0.5, -0.25, 0.25, 0.25, 0.25}
+	},
+	groups = {vessel = 1, dig_immediate = 3, attached_node = 1, drink = 1},
+	on_use = minetest.item_eat(5, "vessels:drinking_glass"),
+	sounds = default.node_sound_glass_defaults()
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "ethereal:lemonade",
+	recipe = {
+		"ethereal:lemon", "group:food_sugar", "group:food_sugar",
+		"vessels:drinking_glass", "group:water_bucket"
+	},
+	replacements = {
+		{"group:water_bucket", "bucket:bucket_empty"},
+	},
+})
+
 -- Olive
 minetest.register_node("ethereal:olive", {
 	description = S("Olive"),
