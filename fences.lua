84afa9ae1c5f48898e07323bdc4bce0c14c9f25a
tenplus1 <kinsellaja@yahoo.com>
2021-03-18 12:06:59 +0000

    have fence posts work with new signs_lib

--- fences.lua
+++ fences.lua
@@ -6,7 +6,8 @@ default.register_fence("ethereal:fence_scorched", {
 	texture = "scorched_tree.png",
 	material = "ethereal:scorched_tree",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
 
 default.register_fence("ethereal:fence_frostwood", {
@@ -14,7 +15,8 @@ default.register_fence("ethereal:fence_frostwood", {
 	texture = "frost_wood.png",
 	material = "ethereal:frost_wood",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
 
 default.register_fence("ethereal:fence_redwood", {
@@ -22,7 +24,8 @@ default.register_fence("ethereal:fence_redwood", {
 	texture = "redwood_wood.png",
 	material = "ethereal:redwood_wood",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
 
 default.register_fence("ethereal:fence_willow", {
@@ -30,7 +33,8 @@ default.register_fence("ethereal:fence_willow", {
 	texture = "willow_wood.png",
 	material = "ethereal:willow_wood",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
 
 default.register_fence("ethereal:fence_yellowwood", {
@@ -38,7 +42,8 @@ default.register_fence("ethereal:fence_yellowwood", {
 	texture = "yellow_wood.png",
 	material = "ethereal:yellow_wood",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
 
 default.register_fence("ethereal:fence_palm", {
@@ -46,7 +51,8 @@ default.register_fence("ethereal:fence_palm", {
 	texture = "moretrees_palm_wood.png",
 	material = "ethereal:palm_wood",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
 
 default.register_fence("ethereal:fence_banana", {
@@ -54,7 +60,8 @@ default.register_fence("ethereal:fence_banana", {
 	texture = "banana_wood.png",
 	material = "ethereal:banana_wood",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
 
 default.register_fence("ethereal:fence_mushroom", {
@@ -62,7 +69,8 @@ default.register_fence("ethereal:fence_mushroom", {
 	texture = "mushroom_trunk.png",
 	material = "ethereal:mushroom_trunk",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
 
 default.register_fence("ethereal:fence_birch", {
@@ -70,7 +78,8 @@ default.register_fence("ethereal:fence_birch", {
 	texture = "moretrees_birch_wood.png",
 	material = "ethereal:birch_wood",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
 
 default.register_fence("ethereal:fence_sakura", {
@@ -78,5 +87,6 @@ default.register_fence("ethereal:fence_sakura", {
 	texture = "ethereal_sakura_wood.png",
 	material = "ethereal:sakura_wood",
 	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = default.node_sound_wood_defaults()
+	sounds = default.node_sound_wood_defaults(),
+	check_for_pole = true
 })
