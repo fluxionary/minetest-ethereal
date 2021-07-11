6d25db1c3fb7f43d49ff1b125a4d007c8166d4d9
tenplus1 <kinsellaja@yahoo.com>
2021-07-11 21:17:55 +0100

    fix coloured candle descriptions

--- extra.lua
+++ extra.lua
@@ -226,20 +226,20 @@ minetest.register_node("ethereal:candle" .. col, {
 end
 
 add_candle("", "")
-add_candle("black", " Black") -- candle colour textures by wRothbard
-add_candle("blue", " Blue")
-add_candle("brown", " Brown")
-add_candle("cyan", " Cyan")
-add_candle("dark_green", " Dark Green")
-add_candle("dark_grey", " Dark Grey")
-add_candle("green", " Green")
-add_candle("grey", " Grey")
-add_candle("magenta", " Magenta")
-add_candle("orange", " Orange")
-add_candle("pink", " Pink")
-add_candle("red", " Red")
-add_candle("violet", " Violet")
-add_candle("yellow", " Yellow")
+add_candle("black", "Black ") -- candle colour textures by wRothbard
+add_candle("blue", "Blue ")
+add_candle("brown", "Brown ")
+add_candle("cyan", "Cyan ")
+add_candle("dark_green", "Dark Green ")
+add_candle("dark_grey", "Dark Grey ")
+add_candle("green", "Green ")
+add_candle("grey", "Grey ")
+add_candle("magenta", "Magenta ")
+add_candle("orange", "Orange ")
+add_candle("pink", "Pink ")
+add_candle("red", "Red ")
+add_candle("violet", "Violet ")
+add_candle("yellow", "Yellow ")
 
 
 -- Wooden Bowl
