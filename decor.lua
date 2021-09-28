9b586feca191e86443edf2489a3caccd31b71f11
tenplus1 <kinsellaja@yahoo.com>
2021-09-28 14:04:48 +0100

    added sunflowers to mapgen

--- decor.lua
+++ decor.lua
@@ -332,13 +332,14 @@ add_node({"default:dirt_with_grass"}, 0.05, {"grassytwo"}, 1, 100,
 	{"farming:carrot_7", "farming:cucumber_4", "farming:potato_3", "farming:vanilla_7",
 	"farming:tomato_7", "farming:corn_8", "farming:coffee_5", "farming:blackberry_4",
 	"farming:raspberry_4", "farming:rhubarb_3", "farming:blueberry_4",
-	"farming:cabbage_6", "farming:lettuce_5"}, nil, nil, nil, ethereal.grassytwo)
+	"farming:cabbage_6", "farming:lettuce_5", "farming:sunflower_8"}, nil, nil, nil,
+	ethereal.grassytwo)
 
 add_node({"default:dirt_with_grass"}, 0.05, {"deciduous_forest"}, 1, 100,
 	{"farming:carrot_7", "farming:cucumber_4", "farming:potato_3", "farming:vanilla_7",
 	"farming:tomato_7", "farming:corn_8", "farming:coffee_5", "farming:blackberry_4",
 	"farming:raspberry_4", "farming:rhubarb_3", "farming:blueberry_4",
-	"farming:beetroot_5"}, nil, nil, nil, ethereal.grassy)
+	"farming:beetroot_5", "farming:sunflower_8"}, nil, nil, nil, ethereal.grassy)
 
 add_node({"default:dirt_with_grass"}, 0.05, {"jumble"}, 1, 100,
 	{"farming:carrot_7", "farming:cucumber_4", "farming:potato_3", "farming:vanilla_7",
@@ -350,7 +351,8 @@ add_node({"ethereal:prairie_dirt"}, 0.05, {"prairie"}, 1, 100,
 	{"farming:carrot_7", "farming:cucumber_4", "farming:potato_3", "farming:parsley_3",
 	"farming:tomato_7", "farming:corn_8", "farming:coffee_5", "farming:blackberry_4",
 	"farming:raspberry_4", "farming:rhubarb_3", "farming:blueberry_4",
-	"farming:pea_5", "farming:beetroot_5"}, nil, nil, nil, ethereal.prairie)
+	"farming:pea_5", "farming:beetroot_5", "farming:sunflower_8"}, nil, nil, nil,
+	ethereal.prairie)
 
 add_node({"ethereal:grove_dirt"}, 0.025, {"mediterranean"}, 1, 100,
 	{"farming:parsley_3"}, nil, nil, nil, ethereal.mediterranean)
