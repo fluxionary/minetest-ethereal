d2d8cb4de85bf1f19923901c028bfb50ca399cd9
tenplus1 <kinsellaja@yahoo.com>
2022-10-29 06:57:48 +0100

    fix spinach and eggplant generation (thx alerikaisattera)

--- decor.lua
+++ decor.lua
@@ -343,14 +343,14 @@ add_node({"default:dirt_with_grass"}, 0.002, {"deciduous_forest"}, 1, 100,
 	"farming:tomato_7", "farming:corn_8", "farming:coffee_5", "farming:blackberry_4",
 	"farming:raspberry_4", "farming:rhubarb_3", "farming:blueberry_4",
 	"farming:beetroot_5", "farming:sunflower_8",
-	"farming:eggplant"}, nil, nil, nil, ethereal.grassy)
+	"farming:eggplant_3"}, nil, nil, nil, ethereal.grassy)
 
 add_node({"default:dirt_with_grass"}, 0.002, {"jumble"}, 1, 100,
 	{"farming:carrot_7", "farming:cucumber_4", "farming:potato_3", "farming:vanilla_7",
 	"farming:tomato_7", "farming:corn_8", "farming:coffee_5", "farming:blackberry_4",
 	"farming:raspberry_4", "farming:rhubarb_3", "farming:blueberry_4",
 	"farming:cabbage_6", "farming:lettuce_5",
-	"farming:spinach"}, nil, nil, nil, ethereal.jumble)
+	"farming:spinach_3"}, nil, nil, nil, ethereal.jumble)
 
 add_node({"ethereal:prairie_dirt"}, 0.025, {"prairie"}, 1, 100,
 	{"farming:carrot_7", "farming:cucumber_4", "farming:potato_3", "farming:parsley_3",
